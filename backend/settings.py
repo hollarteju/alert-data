3b7571904bbe4d5acc0038646307b62e76f95cd6
hollarteju <sirteju2022@gmail.com>
2023-11-01 03:40:32 +0100

    pythonanywhere troubleshooting

--- backend/settings.py
+++ backend/settings.py
@@ -112,23 +112,23 @@ AUTH_USER_MODEL = "alert.UserData"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'zwzrokfn',    # Replace with your database name
-        'USER': 'zwzrokfn',  # Replace with your PostgreSQL username
-        'PASSWORD': '2gLMvVH5_17h8n68d2kTOxxXPrhb6dCs',  # Replace with your PostgreSQL password
-        'HOST': 'trumpet.db.elephantsql.com',  # Set to the PostgreSQL server hostname
-        'PORT': '5432',       # Set to the PostgreSQL server port
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'zwzrokfn',    # Replace with your database name
+#         'USER': 'zwzrokfn',  # Replace with your PostgreSQL username
+#         'PASSWORD': '2gLMvVH5_17h8n68d2kTOxxXPrhb6dCs',  # Replace with your PostgreSQL password
+#         'HOST': 'trumpet.db.elephantsql.com',  # Set to the PostgreSQL server hostname
+#         'PORT': '5432',       # Set to the PostgreSQL server port
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
